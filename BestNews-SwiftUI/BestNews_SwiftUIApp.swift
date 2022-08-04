2052b7d1e898a3556151c97f65c328954e7fbf58
TugrulhanCinar <tugrulhan.cinar@volantx.com>
2022-08-04 11:47:23 +0300

    Initial Commit

---
+++ BestNews-SwiftUI/BestNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestNews_SwiftUIApp.swift
+//  BestNews-SwiftUI
+//
+//  Created by Tugrulhan Çınar on 4.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BestNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
